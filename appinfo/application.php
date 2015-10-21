5174204f212b0f7437e8cb3d8f527fbd6dc61cc2
Pellaeon Lin <nfsmwlin@gmail.com>
2015-10-21 11:11:01 +0800

    Update# get node owner's home folder, exec quota check

--- appinfo/application.php
+++ appinfo/application.php
@@ -15,15 +15,28 @@ use OCP\AppFramework\App;
 
 class Application extends App {
 
-	private function updateFTPQuota($dir) {
-		exec("/usr/local/sbin/pure-quotacheck -d ".escapeshellarg($dir), $output, $return_value);
-		\OCP\Util::writeLog('ftpquota', $dir);
-		return $return_value;
-	}
+	private $servercontainer;
+	private $usermanager;
 
 	public function callback(\OCP\Files\Node $node) {
-		throw new \Exception($node->getPath());
-		\OCP\Util::writeLog('ftpquota', $node->getPath(), \OCP\Util::WARN);
+		$nodepath = $node->getPath();
+		$username = explode('/', $nodepath)[1];
+		try {
+			$user = $this->usermanager->get($username);
+		} catch (\Exception $e) {
+			\OCP\Util::writeLog('ftpquota', 'get user failed: '. $e->getMessage(), \OCP\Util::ERROR);
+			return;
+		}
+		try {
+			$homedir = $user->getHome();
+		} catch (\Exception $e) {
+			\OCP\Util::writeLog('ftpquota', 'get user home failed: '. $e->getMessage(), \OCP\Util::ERROR);
+			return;
+		}
+		exec("/usr/local/sbin/pure-quotacheck -d ".escapeshellarg($homedir.'/files'), $output, $return_value);
+		if ( $return_value !== 0 ) {
+			\OCP\Util::writeLog('ftpquota', 'pure-quotacheck returned '.$return_value.' '.implode("\n", $output), \OCP\Util::ERROR);
+		}
 	}
 
 	public function __construct(array $urlParams=array()){
@@ -31,12 +44,14 @@ class Application extends App {
 
 		$container = $this->getContainer();
 
-		$sc = $container->query('ServerContainer');
+		$this->servercontainer = $container->query('ServerContainer');
+
+		$this->usermanager = $this->servercontainer->getUserManager();
 		// following hooks won't be invoked since file upload or delete through web UI
 		// won't trigger newFile() in  lib/private/files/node/folder.php,
 		// so there is no event emitted
-		$sc->getRootFolder()->listen('\OC\Files', 'postWrite', [$this, 'callback']);
-		$sc->getRootFolder()->listen('\OC\Files', 'postDelete', [$this, 'callback']);
+		$this->servercontainer->getRootFolder()->listen('\OC\Files', 'postWrite', [$this, 'callback']);
+		$this->servercontainer->getRootFolder()->listen('\OC\Files', 'postDelete', [$this, 'callback']);
 		/*$sc->getUserFolder($sc->getUserSession()->getUser()->getUID())->listen('\OC\Files', 'postDelete', function($node) {
 			throw new \Exception($node->getPath());
 			\OCP\Util::writeLog('ftpquota', $node->getPath(), \OCP\Util::WARN);
